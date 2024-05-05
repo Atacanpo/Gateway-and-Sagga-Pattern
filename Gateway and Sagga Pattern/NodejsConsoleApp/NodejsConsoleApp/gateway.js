476fb5d38d499e8b38f48ecc80e4bb65366a8cca
Atacanpo <98129042+Atacanpo@users.noreply.github.com>
2024-05-05 12:02:53 +0300

    Gtaeway and Sagga Pattern

---
+++ Gateway and Sagga Pattern/NodejsConsoleApp/NodejsConsoleApp/gateway.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+const PORT =3001; 
+
+app.use(bodyParser.json());
+
+app.use('/swagger', (req, res) => {
+    res.redirect('http://localhost:5088/swagger/index.html');
+});
+
+app.use('/api', async (req, res) => {
+    try {
+        const originalUrl = `http://localhost:5088${req.url}`;
+        const response = await axios({
+            method: req.method,
+            url: originalUrl,
+            data: req.body,
+            headers: req.headers
+        });
+        res.status(response.status).json(response.data);
+    } catch (error) {
+        res.status(error.response?.status || 500).json({ error: 'Failed to fetch data from original API' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`API Gateway is running on port ${PORT}`);
+});
---
+++ Gateway and Sagga Pattern/NodejsConsoleApp2/NodejsConsoleApp/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 4002;
+
+
+app.use(bodyParser.json());
+
+
+app.post('/QueryTuition', async (req, res) => {
+});
+
+app.post('/StudentPayment', async (req, res) => {
+});
+
+app.post('/SagaExecution', async (req, res) => {
+    try {
+        const tuitionResult = await queryTuition(req.body.studentNumber);
+
+        const paymentResult = await studentPayment(req.body.studentNumber, req.body.period, req.body.paymentTotal);
+
+        res.json({ success: true, message: 'Saga execution successful' });
+    } catch (error) {
+
+        res.status(500).json({ success: false, message: 'Saga execution failed', error: error.message });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
+
+async function queryTuition(studentNumber) {
+}
+
+async function studentPayment(studentNumber, period, paymentTotal) {
+}
